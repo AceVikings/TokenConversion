1884cb80889fc31058380040329cb0c85096b89c
Ace <51465755+AceVikings@users.noreply.github.com>
2022-05-03 11:17:29 +0530

    Update Voyager1.sol

--- contracts/Voyager1.sol
+++ contracts/Voyager1.sol
@@ -17,6 +17,7 @@ contract Voyager1 is Ownable,RaritySigner{
         uint[] tokens;
         uint timestaked;
         uint amount;
+        uint position;
     }
 
     uint public feeBalance;
@@ -57,34 +58,46 @@ contract Voyager1 is Ownable,RaritySigner{
             for(uint j=0;i<inLength;i++){
                 require(PUFF.ownerOf(tokenIds[i][j])==msg.sender,"Not owner");
             }
-            stakeInfo[msg.sender][voyageId[msg.sender]] = tokenInfo(tokenIds[i],block.timestamp,price[i]);
+            stakeInfo[msg.sender][voyageId[msg.sender]] = tokenInfo(tokenIds[i],block.timestamp,price[i],userStaked[msg.sender].length);
+            userStaked[msg.sender].push(voyageId[msg.sender]);
         }
         feeBalance += amount * FEE/100;
         amount += amount * FEE/100;
         require(xGRAV.transferFrom(msg.sender,address(this),amount));
     }
 
-    // function endVoyage(uint[] memory tokenIds) external {
-    //     uint length = tokenIds.length;
-    //     require(length < 60,"Can't end more than 60 puffs");
-    //     uint Grav;
-    //     uint xGrav;
-    //     uint random = uint(vrf());
-    //     for(uint i=0;i<length;i++){
-    //         tokenInfo storage currToken = stakeInfo[tokenIds[i]];
-    //         require(currToken.owner == msg.sender,"Not owner");
-    //         require(block.timestamp - currToken.timestaked >= currToken.amount * 1 days);
-    //         if (random % 100 < 5) {
-    //             Grav += currToken.amount * 1 ether;
-    //         }
-    //         else{
-    //             xGrav += currToken.amount * 1 ether;
-    //         }
-    //         PUFF.transferFrom(address(this),msg.sender,tokenIds[i]);
-    //     }
-    //     GRAV.transfer(msg.sender,Grav);
-    //     xGRAV.transfer(msg.sender,xGrav);
-    // }
+    function endVoyage(uint[] memory voyageIds) external {
+        uint length = voyageIds.length;
+        require(length < 60,"Can't end more than 60 puffs");
+        uint Grav;
+        uint xGrav;
+        uint random = uint(vrf());
+        for(uint i=0;i<length;i++){
+            tokenInfo storage currToken = stakeInfo[msg.sender][voyageIds[i]];
+            require(block.timestamp - currToken.timestaked >= currToken.amount * 1 days);
+            uint inLength = currToken.tokens.length;
+            if (random % 100 < 5) {
+                Grav += currToken.amount * inLength * 1 ether;
+            }
+            else{
+                xGrav += currToken.amount * inLength * 1 ether;
+            }
+            for(uint j=0;i<inLength;i++){
+                PUFF.transferFrom(address(this),msg.sender,currToken.tokens[j]);
+            }
+            popSlot(msg.sender, voyageIds[i]);
+        }
+        GRAV.transfer(msg.sender,Grav);
+        xGRAV.transfer(msg.sender,xGrav);
+    }
+
+    function popSlot(address _user,uint _id) private {
+        uint lastID = userStaked[_user][userStaked[_user].length - 1];
+        uint currentPos = stakeInfo[_user][_id].position;
+        userStaked[_user][currentPos] = lastID;
+        stakeInfo[_user][lastID].position = currentPos;
+        userStaked[_user].pop();
+    }
 
     function vrf() private view returns (bytes32 result) {
         uint256[1] memory bn;
